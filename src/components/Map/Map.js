b512c98661204790bcada318edeaa9be3f504b62
Namrah96 <64134837+Namrah96@users.noreply.github.com>
2020-07-10 17:35:21 +0500

    app.js

---
+++ src/components/Map/Map.js
@@ -0,0 +1,62 @@
+import GoogleMapReact from "google-map-react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './Map.css'
+
+
+const GoogleMap= () => { 
+  
+  const [MapData, setMapData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://corona.lmao.ninja/v2/countries")
+      .then((res) => {
+        setMapData(res.data);
+      })
+      .catch((error) => "Error");
+  }, []);
+
+
+  const map = MapData.map((data, i) => {
+    
+    return (
+    <div key = {i}
+    lat={data.countryInfo.lat}
+        lng={data.countryInfo.long}
+      style={{
+        color:'red',
+        height: '25px',
+        width : '35px',
+        fontWeight: 800,
+        textAlign : 'center',
+        borderRadius: '2px',
+                padding: 2 
+      }}
+    >
+     <img src={data.countryInfo.flag} alt='countryflags' height='10px'/>
+     
+        {data.cases}
+    </div>
+    )})
+  
+
+   return (
+      <div style={{ height: "50vh", width: "100%"}}>
+       {/* <button>Confirmed</button>
+       <button>Recovered</button>
+       <button>Deaths</button> */}
+
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAOvSZKGDxvrOabsPb9f_YUG3_d3I1fcis" }}
+          defaultCenter={{  lat: 30.0, lng: 80.0,}}
+          defaultZoom={4}
+        >
+
+{map}
+        </GoogleMapReact>
+      </div>
+    );
+}
+
+export default Map;
